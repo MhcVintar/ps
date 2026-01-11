eacbd4584b4ac7f9e7dde277c474f13091323a3f
Drsoldier <arne.salobir@gmail.com>
2026-01-11 16:42:32 +0100

    Added functions to handle requests. TODO: 1) Switch FlexView in app for TreeView and fix it up 2) Handle stopping 3) Update Topics on sidebar 4) Test it all

--- razpravljalnica/cmd/client/main.go
+++ razpravljalnica/cmd/client/main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+
+	"context"
 	"fmt"
-	"razpravljalnica/internal/client" 	
+	"razpravljalnica/internal/client"
+	"github.com/urfave/cli/v3"
+	"os"
 )
 
 func main() {
-	fmt.Println("bootstrap")
+	ctx := context.Background()
 
-	if x := client.RunGUI(); x != nil{
-		fmt.Println("ERROR: An error has occured while running the TUI.")
+	cmd := &cli.Command{
+		Name:  "server",
+		Usage: "Start the server",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "address",
+				Aliases:  []string{"a"},
+				Usage:    "Address for the server",
+				Value: "localhost",
+			},
+			&cli.IntFlag{
+				Name: "port",
+				Aliases: []string{"p"},
+				Usage: "Port to connect to server",
+				Value: 8080,
+			},
+		},
+		Action: func(ctx context.Context, c *cli.Command) error {
+			address := c.String("address")
+
+			port := c.Int("port")
+
+			client.Bootstrap(address, port)
+
+			return nil
+		},
+	}
+	if err := cmd.Run(ctx, os.Args); err != nil {
+		fmt.Println("fffff")
 	}
 }
 
--- razpravljalnica/internal/client/client.go
+++ razpravljalnica/internal/client/client.go
@@ -5,6 +5,14 @@ import(
 	"github.com/rivo/tview"
 	"github.com/gdamore/tcell/v2"
 	"log"
+	"razpravljalnica/internal/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"github.com/google/uuid"
+	"time"
+	"context"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"io"
 )
 const(
 	sidebarFocus = 0
@@ -19,19 +27,257 @@ var(
 	msgs *tview.TextView
 	textField *tview.TextArea
 	sendButton *tview.Button
+	pages *tview.Pages
+	server string
+	port int
+	sidebarOrMainFocus int
+	conn *grpc.ClientConn
+	err error
+	client api.MessageBoardClient
+	UUID string
+	idOfClient int64
+	listOfTopics []*api.Topic
+	listOfMessages []*api.Message
+	globalCurrentTopic int64
+	head *api.NodeInfo
+	tail *api.NodeInfo
+	
 )
-func SendMessageToServer(sporocilo string){
-	fmt.Println(sporocilo)
+
+type topicStreamStruct struct{
+	topic *api.Topic
+	streamOfMsg api.MessageBoard_SubscribeTopicClient
+	subscriptionHandleNode *api.NodeInfo
+}
+
+/*	
+ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := client.(ctx, &api.Request{
+		
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+*/
+
+func handleStop(){
+
+	app.Stop()
+}
+
+// TODO
+func updateTopicsOnSidebar(){
+	for indexOfTopic, topic := range listOfTopics{
+
+	}
+}
+
+// Creates a subscription on current topic
+func createSubscription(from int64){
+	// 1. Request a node to which a subscription can be opened
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	response, err := client.GetSubscriptionNode(ctx, &api.SubscriptionNodeRequest{
+		UserId: idOfClient,
+		TopicId: []int64{globalCurrentTopic},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	nodeToSubscribeTo := response.Node
+	subscribeToken := response.SubscribeToken
+	// 2. Subscribe to topic given by previous request
+
+	ctx2, cancel2 := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel2()
+	stream, err := client.SubscribeTopic(ctx2, &api.SubscribeTopicRequest{
+		TopicId: []int64{globalCurrentTopic},
+		UserId: idOfClient,
+		FromMessageId: from,
+		SubscribeToken: subscribeToken,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start a goroutine to read from the stream
+	go func() {
+		for {
+			event, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Println("Stream error:", err)
+				break
+			}
+			// Handle the event
+			log.Printf("Received event: %v on message %d", event.Op, event.Message.Id)
+			// TODO: Update UI or state based on event
+		}
+	}()
+}
+
+
+func createUser(){
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	response, err := client.CreateUser(ctx, &api.CreateUserRequest{
+		Name: UUID,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	idOfClient = response.Id
+}
+
+func likeMessage(sporociloId int64){
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := client.LikeMessage(ctx, &api.LikeMessageRequest{
+		TopicId: globalCurrentTopic,
+		MessageId: sporociloId,
+		UserId: idOfClient,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func sendMessage(sporocilo string){
+	if(len(sporocilo) >= 1){
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		_, err := client.PostMessage(ctx, &api.PostMessageRequest{
+			TopicId: globalCurrentTopic,
+			UserId: idOfClient,
+			Text: sporocilo,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func deleteMessage(sporociloId int64){
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := client.DeleteMessage(ctx, &api.DeleteMessageRequest{
+		TopicId: globalCurrentTopic,
+		UserId: idOfClient,
+		MessageId: sporociloId,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTopic(topicName string){
+	if(len(topicName) >= 1){
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		_, err := client.CreateTopic(ctx, &api.CreateTopicRequest{
+			Name: topicName,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func updateMessage(messageId int64, newText string){
+	if(len(newText) >= 1){
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		_, err := client.UpdateMessage(ctx, &api.UpdateMessageRequest{
+			TopicId: globalCurrentTopic,
+			UserId: idOfClient,
+			MessageId: messageId,
+			Text: newText,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func getTopics(){
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	listOfTopics2, err := client.ListTopics(ctx, &emptypb.Empty{})
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	listOfTopics = listOfTopics2.Topics
+	updateTopicsOnSidebar()
+}
+
+func getMsgs(from int64, limit int32){
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	tmpListOfMessages, err := client.GetMessages(ctx, &api.GetMessagesRequest{
+		TopicId: globalCurrentTopic,
+		FromMessageId: from,
+		Limit: limit,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	listOfMessages = tmpListOfMessages.Messages
 }
 
 
 
+func Bootstrap(serverName string, port int){
+	UUID = uuid.NewString()
+	address := fmt.Sprintf("%s:%d", serverName, port)
+    conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+    if err != nil {
+        log.Fatalf("Failed to connect to %s: %v", address, err)
+    }
+    defer conn.Close()
+
+	client = api.NewMessageBoardClient(conn)
+
+	
 
 
-func RunGUI() error{
-	sidebarOrMainFocus := mainFocus
+	if x:=runGUI(); x!= nil{
+		return
+	}
+}
+
+func handlePopout(){
+	sidebarOrMainFocus = 3
+
+ 	areaForTopic := tview.NewInputField().SetLabel("Topic: ")
+
+
+	newFlexLine := tview.NewFlex().
+		SetDirection(tview.FlexColumn).
+		AddItem(areaForTopic, 100, 10, true)
+
+	newFlexLine.SetInputCapture(func(ev *tcell.EventKey) *tcell.EventKey {
+		if (ev.Key() == tcell.KeyEsc || ev.Key() == tcell.KeyEnter){
+			pages.RemovePage("popup")
+			if(tcell.KeyEnter == ev.Key()){
+				createTopic(areaForTopic.GetText())
+			}
+		}
+
+		return ev
+	})
+	pages.AddAndSwitchToPage("popup", newFlexLine, true)
+	//app.SetFocus(newFlexLine)
+}
+
+func runGUI() error{
+	sidebarOrMainFocus = mainFocus
 	fmt.Println("Client bootstrap")
-	app := tview.NewApplication()
+	app = tview.NewApplication()
 
 	chooseTopic := tview.NewTextView().
 		SetDynamicColors(true).
@@ -44,11 +290,11 @@ func RunGUI() error{
 	chooseTopic.SetBorder(true).SetTitle(" Choose topic ").SetTitleAlign(tview.AlignLeft)
 	exitButton := tview.NewButton("Exit").
 					SetSelectedFunc(func() {
-						app.Stop()
+						handleStop()
 					})
 	addTopicButton := tview.NewButton("Add topic").
 					SetSelectedFunc(func() {
-						app.Stop()
+						handlePopout()
 					})
 	rowButtons := tview.NewFlex().
 		SetDirection(tview.FlexColumn).
@@ -74,7 +320,7 @@ func RunGUI() error{
 	sendButton := tview.NewButton("Send").
 					SetSelectedFunc(func() {
 						textToSend := textField.GetText()
-						SendMessageToServer(textToSend)
+						sendMessage(textToSend)
 					})
 	sendRow := tview.NewFlex().
 		SetDirection(tview.FlexColumn).
@@ -118,7 +364,7 @@ func RunGUI() error{
 							app.SetFocus(exitButton)
 						}
 					}else{
-						app.Stop()
+						handleStop()
 						log.Fatal("Fatal error 100: tried getting focus in sidebar, did not get exitButton or chooseTopic")
 					}
 				}else if (sidebarOrMainFocus == mainFocus){
@@ -129,17 +375,28 @@ func RunGUI() error{
 					}else if(app.GetFocus() == sendButton){
 						app.SetFocus(msgs)
 					}else{
-						app.Stop()
+						handleStop()
 						log.Fatal("Fatal error 101: tried getting focus in main, did not get msgs or sendText")
 					}
 				}
 				return event
 		}
+		switch event.Rune(){
+			case 'd', 'D':{
+				if (app.GetFocus() == exitButton){
+					app.SetFocus(addTopicButton)
+				}		
+			}
+			case 'a', 'A':{
+				if(app.GetFocus() == addTopicButton){
+					app.SetFocus(exitButton)
+				}
+			}
+		}
+
 		return event
 	})
-	return app.SetRoot(layout, true).Run()
-}
-
-func changeFooterToStop(footer *tview.TextView){
-	
+	pages = tview.NewPages()
+	pages.AddPage("main", layout, true, true)
+	return app.SetRoot(pages, true).Run()
 }
