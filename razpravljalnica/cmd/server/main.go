d63fe9f26bc9cc78ffad0988501537ad65a62507
MhcVintar <mihavintar3@gmail.com>
2026-01-12 10:50:58 +0100

    rolled back server cmd

--- razpravljalnica/cmd/server/main.go
+++ razpravljalnica/cmd/server/main.go
@@ -31,44 +31,19 @@ func main() {
 				Name:  "downstream-address",
 				Usage: "Address of the downstream server",
 			},
-			&cli.BoolFlag{
-				Name: "automatic-setup",
-				Usage: "Set up servers automatically",
-				Value: false,
-			},
-			&cli.IntFlag{
-				Name: "server-count",
-				Usage: "This flag is used when automatic-setup flag is set to true. If this isn't set, it defaults to 1",
-				Value: 1,
-			},
-		
 		},
 		Action: func(ctx context.Context, command *cli.Command) error {
 			address := command.String("address")
-			if(!command.Bool("automatic-setup")){
-				var downstreamID *string
-				if command.IsSet("downstream-id") {
-					downstreamID = shared.AnyPtr(command.String("downstream-id"))
-				}
-				var downstreamAddress *string
-				if command.IsSet("downstream-address") {
-					downstreamAddress = shared.AnyPtr(command.String("downstream-address"))
-				}
-			}else{
-				
-				for i = port; i-port <= command.Int("server-count"); i++{
-					
-				}
-				var downstreamID *string
-				if command.IsSet("downstream-id") {
-					downstreamID = shared.AnyPtr(command.String("downstream-id"))
-				}
-				var downstreamAddress *string
-				if command.IsSet("downstream-address") {
-					downstreamAddress = shared.AnyPtr(command.String("downstream-address"))
-				}
+
+			var downstreamID *string
+			if command.IsSet("downstream-id") {
+				downstreamID = shared.AnyPtr(command.String("downstream-id"))
 			}
-			
+			var downstreamAddress *string
+			if command.IsSet("downstream-address") {
+				downstreamAddress = shared.AnyPtr(command.String("downstream-address"))
+			}
+
 			server, err := server.NewServerNode(address, downstreamID, downstreamAddress)
 			if err != nil {
 				return err
